040c180d2ae879def76bf2bcd40c19b19dda2fbf
sfdevelop <sfdevelop747@gmail.com>
2020-10-06 00:57:33 +0300

    06-10

--- assets/js/main.js
+++ assets/js/main.js
@@ -17827,6 +17827,41 @@ $(document).ready(function () {
 		]
 	  });
 });
+// cслайдер новинки
+$(document).ready(function () {
+	$('.slider__card').slick({
+		dots: false,
+		infinite: false,
+		speed: 300,
+		slidesToShow: 3,
+		slidesToScroll: 1,
+		responsive: [
+		  {
+			breakpoint: 992,
+			settings: {
+			  slidesToShow: 3,
+			  slidesToScroll: 3,
+			  infinite: true,
+			  dots: true
+			}
+		  },
+		  {
+			breakpoint: 768,
+			settings: {
+			  slidesToShow: 2,
+			  slidesToScroll: 2
+			}
+		  },
+		  {
+			breakpoint: 480,
+			settings: {
+			  slidesToShow: 1,
+			  slidesToScroll: 1
+			}
+		  }
+		]
+	  });
+});
 module.exports = lazyload;
 
 var inViewport = require('in-viewport');
